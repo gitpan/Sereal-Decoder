1893da20c5d265b1ab5104bc4ea6f1baf1cc3777
Steffen Müller <smueller@cpan.org>
2012-09-13 15:19:16 +0000

    Import of SMUELLER/Sereal-Decoder-0.08 from CPAN.
    gitpan-cpan-distribution: Sereal-Decoder
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         SMUELLER/Sereal-Decoder-0.08.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Sereal/Decoder.pm
+++ lib/Sereal/Decoder.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use Exporter 'import';
 
 our @EXPORT_OK = qw(decode_sereal);
@@ -132,7 +132,7 @@ is concatenated into the same string (code not very robust...):
   my $pos = 0;
   eval {
     while (1) {
-      push @out, $decoder->decode_with_offset($sereal_string, 0);
+      push @out, $decoder->decode_with_offset($sereal_string, $pos);
       $pos += $decoder->bytes_consumed;
       last if $pos >= length($sereal_string)
            or not $decoder->bytes_consumed;
